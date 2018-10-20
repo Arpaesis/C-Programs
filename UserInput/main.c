0606e3ed59f14ff197bbc4afdf04c3f19f114a74
Arpaesis <arpaesis@gmail.com>
2018-10-20 13:30:14 -0400

    Added a user input example.

---
+++ UserInput/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Create a variable to store the number.
+    int input;
+
+    printf("Please enter a number: ");
+
+    //Takes the next input and stores it at the address of var input.
+    //This initializes var input for us.
+    scanf("%d", &input);
+
+    printf("the number you gave is: %d\n", input);
+    return 0;
+}
